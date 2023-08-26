cb74252f42a00973ba2e8f373a210d078fa7a2a2
Mohamad Supangat <hantamkoding@gmail.com>
2023-08-26 11:46:47 +0700

    add: plugin: ziggy for routing helper in inertia.js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,12 @@
 
 <head>
     <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+
+    @route
     @inertiaHead
+    @vite('resources/js/app.js')
 </head>
 
 <body>
